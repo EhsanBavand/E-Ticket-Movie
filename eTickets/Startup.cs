05851c2e8a764cdbb197d0b48729abcefedbf8a7
Ehsan Bavand <ehsan.bavand@ardene.com>
2022-06-20 15:09:13 -0400

    Add classes, migration

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,9 +1,12 @@
+using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +26,11 @@ namespace eTickets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbCOntext Configuration
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString
+                ("DefaultConnectionString")));
+
+
             services.AddControllersWithViews();
         }
 
